a9ff259a79dfdb758a360bc06541af89ddfb4d88
asifulmamun <asifulmamun@gmail.com>
2024-04-24 10:17:26 +0600

    Fix: Database connection systex and session registered systex also db information as per localhost, db info will be change as per required like live host.

--- database_connection.php
+++ database_connection.php
@@ -1,10 +1,22 @@
 <?php
-$connect = mysql_connect('localhost', 'rashed', 'rashed');
-mysql_select_db('inventory', $connect);
+// $connect = mysql_connect('localhost', 'rashed', 'rashed');
+// mysql_select_db('inventory', $connect);
+$servername = "localhost";
+$username = "asifulmamun";
+$password = "#Asiful1998";
+$database = "inventory";
+$mysqli = new mysqli($servername, $username, $password, $database);
+
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
+
 
 session_start();
 
 // Register session variables (deprecated)
-session_register('type');
-session_register('user_id');
-?>
+// session_register('type');
+// session_register('user_id');
+$_SESSION['type'] = '';
+$_SESSION['user_id'] = '';
---
+++ documentation/changelog.md
@@ -0,0 +1,3 @@
+#### Connection of DB
+- Create folder to root 'documentation' task pdf file and changelog.md for log of work
+- Fix: Database connection systex and session registered systex also db information as per localhost, db info will be change as per required like live host.
